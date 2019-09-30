646b5f0b7dc5a94bb8a579d6f9adefa9625e9c0c
Dorin Balan <dbalan@itron.com>
2019-09-30 21:02:33 +0200

    Global exception handler

---
+++ AmazingCo/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace AmazingCo.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (StatusCodeException ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, StatusCodeException exception)
+        {
+            string result = null;
+            context.Response.ContentType = "application/json";
+            if (exception is StatusCodeException)
+            {
+                result = new ErrorDetails() { Message = exception.Message, StatusCode = (int)exception.StatusCode }.ToString();
+                context.Response.StatusCode = (int)exception.StatusCode;
+            }
+            else
+            {
+                result = new ErrorDetails() { Message = "Runtime Error", StatusCode = (int)HttpStatusCode.BadRequest }.ToString();
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            }
+            return context.Response.WriteAsync(result);
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            string result = new ErrorDetails() { Message = exception.Message, StatusCode = (int)HttpStatusCode.InternalServerError }.ToString();
+            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
---
+++ AmazingCo/Middlewares/StatusCodeException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace AmazingCo.Middlewares
+{
+    public class StatusCodeException : Exception
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public string ContentType { get; set; } = @"text/plain";
+
+        public StatusCodeException(HttpStatusCode statusCode)
+        {
+            this.StatusCode = statusCode;
+        }
+
+        public StatusCodeException(HttpStatusCode statusCode, string message) : base(message)
+        {
+            this.StatusCode = statusCode;
+        }
+
+        public StatusCodeException(HttpStatusCode statusCode, Exception inner) : this(statusCode, inner.ToString()) { }
+
+        public StatusCodeException(HttpStatusCode statusCode, JObject errorObject) : this(statusCode, errorObject.ToString())
+        {
+            this.ContentType = @"application/json";
+        }
+    }
+}
