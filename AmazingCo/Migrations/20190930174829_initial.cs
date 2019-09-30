6f453fc672e82e3a64d4ba697cf37634ea992c05
Dorin Balan <dbalan@itron.com>
2019-09-30 21:01:43 +0200

    Enable migrations

---
+++ AmazingCo/Migrations/20190930174829_initial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AmazingCo.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Nodes",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    ParentId = table.Column<string>(nullable: true),
+                    RootId = table.Column<string>(nullable: true),
+                    Height = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nodes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Nodes");
+        }
+    }
+}
