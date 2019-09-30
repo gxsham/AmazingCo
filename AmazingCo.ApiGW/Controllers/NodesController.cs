ed277f4847474d4ef5af440e1d835dc5e47807d5
Dorin Balan <dbalan@itron.com>
2019-09-30 21:32:03 +0200

    Adding API Gateway with Swagger

---
+++ AmazingCo.ApiGW/Controllers/NodesController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AmazingCo.ApiGW.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NodesController : ControllerBase
+    {
+        
+        [HttpGet("{id}/getChildren")]
+        public string GetChildren(int id)
+        {
+            return string.Empty;
+        }
+        
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+
+        }
+    }
+}
