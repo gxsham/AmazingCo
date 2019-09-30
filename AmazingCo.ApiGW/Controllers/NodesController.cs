ffdc3b94e914e634364b38c7c17c857437b3d64a
Dorin Balan <dbalan@itron.com>
2019-10-01 00:00:18 +0200

    Api Gw Nodes Controller

--- AmazingCo.ApiGW/Controllers/NodesController.cs
+++ AmazingCo.ApiGW/Controllers/NodesController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Threading.Tasks;
+using AmazingCo.ApiGW.Clients;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AmazingCo.ApiGW.Controllers
@@ -7,17 +8,42 @@ namespace AmazingCo.ApiGW.Controllers
     [ApiController]
     public class NodesController : ControllerBase
     {
+        private readonly INodeClient _nodeClient;
+        public NodesController(INodeClient nodeClient)
+        {
+            _nodeClient = nodeClient ?? throw new System.ArgumentNullException(nameof(nodeClient));
+        }
         
         [HttpGet("{id}/getChildren")]
-        public string GetChildren(int id)
+        public async Task<IActionResult> GetChildren(string id)
         {
-            return string.Empty;
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                return BadRequest("Parent id cannot be null.");
+            }
+            var response = await _nodeClient.GetChildrenAsync(id);
+            if (response.IsSuccessStatusCode)
+            {
+                return Ok(await response.Content.ReadAsStringAsync());
+            } 
+            
+            return BadRequest(response);
         }
         
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> Put(string id, string parentId)
         {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                return BadRequest("Node id cannot be null.");
+            }
+            var response = await _nodeClient.ChangeParentAsync(id, parentId);
+            if (response.IsSuccessStatusCode)
+            {
+                return NoContent();
+            }
 
+            return BadRequest(response);
         }
     }
 }
